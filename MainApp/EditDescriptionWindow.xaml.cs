13ab354569457563d6409885247857e93fe1b643
Salads <dgentry.email@protonmail.com>
2026-08-09 00:37:15 -0700

    Finish Description functionality in NewPostingWindow
    - Added EditDescriptionWindow and interactability
    - Update charcount label when description updates
    - Description has multi-line support, but plaintext. Will convert into rich text for hyperlink support in main UI.

---
+++ MainApp/EditDescriptionWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainApp
+{
+    /// <summary>
+    /// Interaction logic for EditDescriptionWindow.xaml
+    /// </summary>
+    public partial class EditDescriptionWindow : Window
+    {
+        public EditDescriptionWindow(string description)
+        {
+            InitializeComponent();
+            descTextBox.Text = description;
+        }
+
+        private void descSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void descCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
--- MainApp/NewPostingWindow.xaml.cs
+++ MainApp/NewPostingWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace MainApp
     /// </summary>
     public partial class NewPostingWindow : Window
     {
+        JobPosting m_jobPosting = new JobPosting();
+
         public NewPostingWindow()
         {
             InitializeComponent();
@@ -31,10 +33,21 @@ namespace MainApp
 
         private void descButton_Click(object sender, RoutedEventArgs e)
         {
+            EditDescriptionWindow editDescWindow = new EditDescriptionWindow(m_jobPosting.JobDescription)
+            {
+                Owner = this
+            };
+            editDescWindow.ShowDialog();
 
+            int newCharCount = 0;
+            if(editDescWindow.DialogResult == true)
+            {
+                newCharCount = editDescWindow.descTextBox.Text.Length;
+                m_jobPosting.JobDescription = editDescWindow.descTextBox.Text;
+            }
+
+            descTextBlock.Text = $"({newCharCount} chars)";
         }
-
-        JobPosting m_jobPosting = new JobPosting();
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
