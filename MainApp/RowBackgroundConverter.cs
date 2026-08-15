f225bd004e57f4369d5f3c867c52c3b05c2591d1
Salads <dgentry.email@protonmail.com>
2026-08-15 16:06:00 -0700

    Added conditional row coloring based on job status

---
+++ MainApp/RowBackgroundConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using MainApp.Models;
+using System.Windows.Media;
+
+namespace MainApp
+{
+    class RowBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Brush result = Brushes.White;
+            JobStatus status = (JobStatus)value;
+
+            switch (status)
+            {
+                case JobStatus.Applied:
+                    result = Brushes.Beige;
+                    break;
+                case JobStatus.Accepted:
+                    result = Brushes.LightGreen;
+                    break;
+                case JobStatus.Interviewing:
+                    result = Brushes.LightBlue;
+                    break;
+                case JobStatus.Rejected:
+                    result = Brushes.PaleVioletRed;
+                    break;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
